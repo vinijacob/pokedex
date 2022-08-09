6a8c98742f6c70c089b781b13ba45381bd757a7a
Vinícius Ramos Jacob <vini_jacob@hotmail.com>
2022-08-09 11:33:36 -0400

    Criação da pokedex

---
+++ main.js
@@ -0,0 +1,39 @@
+let pokeNumber = 2
+let url = `https://pokeapi.co/api/v2/pokemon/${pokeNumber}`
+
+console.log(url)
+
+function getPokemon() {
+    fetch(url)
+    .then((response) => {
+        return response.json();
+    })
+
+    .then((data) => {
+        console.clear()
+        console.log(data)
+
+        document.querySelector('.poke_name').innerHTML = data['name'].toUpperCase()
+        document.querySelector('.poke__peso').innerHTML = `Peso: ${data['weight']}kg`
+        document.querySelector('.poke__altura').innerHTML = `Altura: ${data['height']}m`
+
+        let img = data['sprites']['front_default']
+        document.querySelector('.poke_img').setAttribute('src', img)
+    })
+
+
+}
+
+getPokemon()
+
+document.querySelector('.btn_forw').onclick = () => {
+    console.log(url)
+    pokeNumber++;
+    console.log(pokeNumber)
+}
+
+document.querySelector('.btn_backw').onclick = () => {
+    console.log(url)
+    pokeNumber--;
+    console.log(pokeNumber)
+}
